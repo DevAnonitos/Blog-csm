71f55f7b049ff33e039f6117e0f20316d76a7198
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-25 23:56:01 +0700

    update-src

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div key={index}>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+
+            </div>
+        </div>
+        
       </div>
     </div>
   )
