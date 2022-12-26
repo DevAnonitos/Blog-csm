4e33648a356e16b02db4cc7ce67f80890fe6d3ef
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-26 10:47:43 +0700

    update-src

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as PostCard } from "./PostCard";
+export { default as Categories } from "./Categories";
+export { default as PostWidget } from "./PostWidget";
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import { PostCard, PostWidget, Categories } from "../components";
+
 const posts = [
   {title: "React testing", excerpt:   "Learn React testing"},
   {title: "React with Tailwind", excerpt: "Learn React with Tailwind"}
@@ -16,16 +18,14 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div key={index}>
-              {post.title}
-              {post.excerpt}
-            </div>
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
-
+              <PostWidget />
+              <Categories />
             </div>
         </div>
         
