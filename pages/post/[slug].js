4d769a6489c30a889dc6863a617aee7cd96af43a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-29 16:51:46 +0700

    update-Component@2

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -12,10 +12,14 @@ const PostDetails = () => {
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
-
+                    <PostDetail />
+                    <Author />
+                    <CommentsForm />
+                    <Comments />
                 </div>
                 <div className="col-span-1 lg:col-span-4">
-
+                    <PostWidget />
+                    <Categories />
                 </div>
             </div>
 
