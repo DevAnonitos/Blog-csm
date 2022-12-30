3209844b0ab62f48f9e50683dcc9400e5cbb3d6b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-30 21:20:26 +0700

    update-CommentsPost

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
 
-  const [error, setError] = useState([]);
+  const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const commentEl = useRef();
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  const handleCommentSubmit = () =>{
+
+  }
 
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
@@ -25,11 +29,32 @@ const CommentsForm = () => {
 
         </textarea>
       </div>
-      <div className='grid grid-cols-1 gap-4 mb-4'>
-        
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+        <input 
+          type="text" 
+          ref={nameEl} 
+          placeholder='Name'
+          name='name'
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
+        <input 
+          type="text" 
+          ref={nameEl} 
+          placeholder='Email'
+          name='email'
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
       </div>
-      <div className='grid grid-cols-1 gap-4 mb-4'>
-        
+      {error && <p className='text-xs text-red-600'>All fields are required</p>}
+      <div className='flex align-middle justify-center mt-8'>
+        <button
+          type='button'
+          onClick={handleCommentSubmit}
+          className='transition duration-500 ease hover:bg-indigo-400 inline-block  bg-pink-500 hover:text-white text-lg rounded-full px-10 py-4 cursor-pointer'
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && <span className='text-xl float-right font-semibold mt-3 text-green-500'>Comment submitted for review</span>}
       </div>
     </div>
   )
