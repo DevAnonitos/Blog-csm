6aae3e56a954b5fe2f28d7fe0960a6b81f994e7c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-30 16:29:25 +0700

    update-Author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div className='text-center mt-20 mb-8 p-14 relative rounded-lg bg-cyan-100 bg-opacity-40'>
+      <div className='absolute left-0 right-2 -top-5'>
+        <img
+          alt={author.name}
+          height='100px'
+          width='100px'
+          className='align-middle rounded-full'
+          src={author.photo.url}
+        />
+        <h3 className='text-white my-0 text-xl font-bold'>{author.name}</h3>
+        <p className='text-white text-lg'>{author.bio}</p>
+      </div>
+    </div>
   )
 }
 
